5067015e867344cdb09c14f09cd67ab40f39b294
rzbro <ry0102barbosa@gmail.com>
2026-04-08 12:23:20 +0000

    versão1

---
+++ stringo.js
@@ -0,0 +1,6 @@
+let idade = 17;
+let dataNascimento =  2009;
+
+//console.log("Minha idade é: " + idade)
+//console.log("Meu ano de nascimento é:" + dataNascimento);
+console.log(`Minha idade é: ${idade} e meu ano de nascimento é: ${dataNascimento}`);
