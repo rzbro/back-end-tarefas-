4716e6b1456f7036b3107b4f6c446b330d564174
rzbro <ry0102barbosa@gmail.com>
2026-04-08 12:22:01 +0000

    versão2

---
+++ relatorio.js
@@ -0,0 +1,13 @@
+//Relatório de notas
+console.log("Relatório de Notas");
+
+let aluno = "Francisco";
+let nota1 = 8;
+let nota2 = 7;
+let nota3 = 4;
+let media = (nota1 + nota2 + nota3) / 3;
+
+//let relatorio = "Aluno: " + aluno + "\n" + "Notas: " + nota1 + " e " + nota2 + " | Média: " + media;
+let relatorio = "Aluno: " + aluno + " | Notas: " + nota1 + " e " + nota2 + " | Média: " + media;
+
+console.log(relatorio)
