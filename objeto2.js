4b74f6d601e3064460af0d32560d4728e94f0b4e
rzbro <ry0102barbosa@gmail.com>
2026-04-27 11:55:11 +0000

    versão1

---
+++ objeto2.js
@@ -0,0 +1,11 @@
+const Paodequeijo = {
+    material: "polvilho azedo e queijo",
+    cor: "amarelo",
+    sabor: "queijo acentuado com polvilho",
+    tempoForno:"cerca de 35-45 minutos"
+    assado:"false"
+}
+console.log ("doque é feito?"material.Paodequeijo)
+
+Paodequeijo assado= true;
+console.log(Paodequeijo);
