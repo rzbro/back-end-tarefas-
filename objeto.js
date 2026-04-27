c9fc226db0ec9f72e50ce7c8cb12d808ed7d3aa8
rzbro <ry0102barbosa@gmail.com>
2026-04-27 11:54:22 +0000

    v1

---
+++ objeto.js
@@ -0,0 +1,13 @@
+const bancoAutomotivo = {
+    material: "Couro Natural",
+    cor: "caramelo",
+    tamanho: "padrão",
+    concluido: false
+}
+console.log ("material escolhido",bancoAutomotivo.material);
+
+bancoAutomotivo.concluido = true;
+console.log(bancoAutomotivo);
+
+
+
